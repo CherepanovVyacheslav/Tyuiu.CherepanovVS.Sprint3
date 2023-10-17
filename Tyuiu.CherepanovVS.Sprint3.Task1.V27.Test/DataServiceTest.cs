af8cf0453992de3bf803835e2c4235ef009438bc
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-10-17 20:31:57 +0500

    Task1

---
+++ Tyuiu.CherepanovVS.Sprint3.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.CherepanovVS.Sprint3.Task1.V27.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint3.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 5;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 419028.1;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.CherepanovVS.Sprint3.Task1.V27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint3.Task1.V27.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint3.Task1.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт 3                                                                  *");
+            Console.WriteLine("*Тема: Оператор цикла while                                                *");
+            Console.WriteLine("*Задание #1                                                                *");
+            Console.WriteLine("*Вариант #27                                                               *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение  *");
+            Console.WriteLine("* ряда по формуле, при n=0,75                                              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 5;
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadLine();
+        }
+    }
+}
