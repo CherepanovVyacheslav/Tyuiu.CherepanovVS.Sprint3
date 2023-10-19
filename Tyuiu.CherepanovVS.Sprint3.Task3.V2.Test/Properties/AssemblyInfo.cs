668f859f21723ef8347db8b582018e3487ae5ade
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-10-19 20:49:41 +0500

    Task3

---
+++ Tyuiu.CherepanovVS.Sprint3.Task3.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.CherepanovVS.Sprint3.Task3.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.CherepanovVS.Sprint3.Task3.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f43f28eb-4de5-4b1b-af6b-55fd5db4b35b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.CherepanovVS.Sprint3.Task3.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint3.Task3.V2.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint3.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт 3                                                                  *");
+            Console.WriteLine("*Тема: Оператор цикла foreach                                              *");
+            Console.WriteLine("*Задание #3                                                                *");
+            Console.WriteLine("*Вариант #2                                                                *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Используя цикл foreach подсчитать максимальное количество букв z,        *");
+            Console.WriteLine("* находящихся на соседних позициях в строке: asdzzz vfvfzz v gthvz         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            string value = "asdzzz vfvfzz v gthvz";
+            char chr = 'z';
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Колличество символов = " + ds.GetMaxCharCount(value,chr));
+            Console.ReadLine();
+        }
+    }
+    
+}
